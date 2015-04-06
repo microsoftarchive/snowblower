55bf983eb69da670f13ce0832242e9565ff0d999
James Duncan Davidson <duncandavidson@me.com>
2015-04-06 13:47:07 +0200

    Remove extrania

--- snowplow/payload.go
+++
@@ -1,9 +0,0 @@
-package snowplow
-
-import "encoding/json"
-
-// Payload defines the structure of data posted from Snowplow trackers
-type Payload struct {
-	Schema string            `json:"schema"`
-	Data   []json.RawMessage `json:"data"`
-}
