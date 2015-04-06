5d6dac3d104f8398f69bdf9dbb8788e10a0b2fda
James Duncan Davidson <duncandavidson@me.com>
2015-04-06 13:46:33 +0200

    Handle XFF headers

---
+++ request.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+var privateIPMasks = func() []net.IPNet {
+	masks := []net.IPNet{}
+	for _, m := range []string{
+		"192.168.0.0/16",
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"fc00::/7",
+	} {
+		_, n, _ := net.ParseCIDR(m)
+		masks = append(masks, *n)
+	}
+	return masks
+}()
+
+func isPublicIP(ip net.IP) bool {
+	for _, m := range privateIPMasks {
+		if m.Contains(ip) {
+			return false
+		}
+	}
+	return true
+}
+
+func realRemoteAddr(request *http.Request) string {
+	if header := request.Header.Get("X-Forwarded-For"); header != "" {
+		for _, addy := range strings.Split(header, ",") {
+			addy = strings.TrimSpace(addy)
+			ip := net.ParseIP(addy)
+			if ip != nil && isPublicIP(ip) {
+				return addy
+			}
+		}
+	}
+
+	return request.RemoteAddr
+}
---
+++ request_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func makeRequest() *http.Request {
+	request, _ := http.NewRequest("GET", "/", nil)
+	request.RemoteAddr = "1.2.3.4"
+	return request
+}
+
+func TestRealRemoteAddrWithNoXFFHeader(t *testing.T) {
+	request := makeRequest()
+	addr := realRemoteAddr(request)
+	if addr != "1.2.3.4" {
+		t.Errorf("Expected 1.2.3.4, got: %s", addr)
+	}
+}
+
+func TestRealRemoteAddrWithSimpleXFFHeader(t *testing.T) {
+	request := makeRequest()
+	request.Header.Set("X-Forwarded-For", "9.9.9.9")
+	addr := realRemoteAddr(request)
+	if addr != "9.9.9.9" {
+		t.Errorf("Expected 9.9.9.9, got: %s", addr)
+	}
+}
+
+func TestRealRemoteAddrWithJunkXFFHeader(t *testing.T) {
+	request := makeRequest()
+	request.Header.Set("X-Forwarded-For", "JUNK")
+	addr := realRemoteAddr(request)
+	if addr != "1.2.3.4" {
+		t.Errorf("Expected 1.2.3.4, got: %s", addr)
+	}
+}
+
+func TestRealRemoteAddrWithComplexXFFHeader(t *testing.T) {
+	request := makeRequest()
+	request.Header.Set("X-Forwarded-For", "9.9.9.9, 8.8.8.8")
+	addr := realRemoteAddr(request)
+	if addr != "9.9.9.9" {
+		t.Errorf("Expected 9.9.9.9, got: %s", addr)
+	}
+}
+
+func TestRealRemoteAddrWithPrivate10DotXFFHeader(t *testing.T) {
+	request := makeRequest()
+	request.Header.Set("X-Forwarded-For", "10.0.0.2, 8.8.8.8")
+	addr := realRemoteAddr(request)
+	if addr != "8.8.8.8" {
+		t.Errorf("Expected 8.8.8.8, got: %s", addr)
+	}
+}
+
+func TestRealRemoteAddrWithPrivate172DotXFFHeader(t *testing.T) {
+	request := makeRequest()
+	request.Header.Set("X-Forwarded-For", "172.16.0.2, 8.8.8.8")
+	addr := realRemoteAddr(request)
+	if addr != "8.8.8.8" {
+		t.Errorf("Expected 8.8.8.8, got: %s", addr)
+	}
+}
+
+func TestRealRemoteAddrWithPrivate192DotXFFHeader(t *testing.T) {
+	request := makeRequest()
+	request.Header.Set("X-Forwarded-For", "192.168.2.2, 8.8.8.8")
+	addr := realRemoteAddr(request)
+	if addr != "8.8.8.8" {
+		t.Errorf("Expected 8.8.8.8, got: %s", addr)
+	}
+}
