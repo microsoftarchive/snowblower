2e2c15cd9ba1a6295436dc3e8460709335600dc0
James Duncan Davidson <duncandavidson@me.com>
2015-04-08 20:51:52 +0200

    Move main server function to collector

--- collector.go
+++ collector.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"time"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/service/sns"
 
 	"code.google.com/p/go-uuid/uuid"
 )
@@ -95,4 +100,50 @@ func (c *collector) servePost(
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(nil)
+}
+
+func startCollector() {
+	if os.Getenv("GOMAXPROCS") == "" {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	var credentials aws.CredentialsProvider
+	if os.Getenv("AWS_ACCESS_KEY_ID") != "" {
+		credentials = aws.DefaultCreds()
+	} else {
+		credentials = aws.IAMCreds()
+	}
+
+	snsTopic := os.Getenv("SNS_TOPIC")
+	if snsTopic == "" {
+		panic("SNS_TOPIC required")
+	}
+
+	snsService := sns.New(&aws.Config{
+		Credentials: credentials,
+		Region:      "eu-west-1",
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	healthHandler := &health{}
+	http.Handle("/api/health", healthHandler)
+	http.Handle("/health", healthHandler)
+
+	collectorHandler := &collector{
+		publisher: &SNSPublisher{
+			service: snsService,
+			topic:   snsTopic,
+		},
+	}
+
+	http.Handle("/com.snowplowanalytics.snowplow/tp2", collectorHandler)
+	http.Handle("/i", collectorHandler)
+
+	portString := fmt.Sprintf(":%s", port)
+	log.Printf("Starting server on %s", portString)
+	http.ListenAndServe(portString, nil)
 }
