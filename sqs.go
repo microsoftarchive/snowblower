492a1545b609f4b0b59201bc135fb0241c2760b0
James Duncan Davidson <duncandavidson@me.com>
2015-04-08 20:52:05 +0200

    Adding SQS

---
+++ sqs.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/service/sqs"
+)
+
+// SQSService ...
+type SQSService struct {
+	service *sqs.SQS
+	url     string
+}
+
+func (service *SQSService) getMessages() ([]*sqs.Message, error) {
+	params := &sqs.ReceiveMessageInput{
+		QueueURL: aws.String(service.url),
+		AttributeNames: []*string{
+			aws.String("SentTimestamp"),
+		},
+		MaxNumberOfMessages: aws.Long(10),
+		MessageAttributeNames: []*string{
+			aws.String("All"),
+		},
+		VisibilityTimeout: aws.Long(60),
+		WaitTimeSeconds:   aws.Long(20),
+	}
+	response, err := service.service.ReceiveMessage(params)
+
+	if awserr := aws.Error(err); awserr != nil {
+		// A service error occurred.
+		fmt.Println("Error:", awserr.Code, awserr.Message)
+	} else if err != nil {
+		// A non-service error occurred.
+		panic(err)
+	}
+
+	if response != nil {
+		return nil, err
+	}
+	return response.Messages, nil
+}
