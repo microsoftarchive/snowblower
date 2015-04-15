4510cab5e5cfba0dbb17ebc26edf1a17aec8dbb0
James Duncan Davidson <duncandavidson@me.com>
2015-04-15 19:53:36 +0200

    Add ETL command

--- main.go
+++ main.go
@@ -12,8 +12,17 @@ func main() {
 		},
 	}
 
+	var etlCmd = &cobra.Command{
+		Use:   "etl",
+		Short: "Run the ETL processor",
+		Run: func(cd *cobra.Command, args []string) {
+			startETL()
+		},
+	}
+
 	var rootCmd = &cobra.Command{Use: "snowblower"}
 	rootCmd.AddCommand(collectorCmd)
+	rootCmd.AddCommand(etlCmd)
 	rootCmd.Execute()
 
 }
